547a49e69fe87c0c7d9ce852ebdeb475ecec3e16
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-13 20:05:29 +0530

    style: prettier .eslintrc.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,31 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    'plugin:react-hooks/recommended',
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "tailwind.config.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
-}
+      version: "detect",
+    },
+  },
+};
